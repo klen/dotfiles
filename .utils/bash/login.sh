43353a1d0e0a272f425d625ca0951edb8f08511d
Kirill Klenov <horneds@gmail.com>
2012-12-24 19:32:57 +0400

    Fix login screen

--- .utils/bash/login.sh
+++ .utils/bash/login.sh
@@ -4,5 +4,5 @@
 echo    -e ${yellow}------------------------------ $NC
 echo    -e ${CYAN}This is BASH ${RED}${BASH_VERSION%.*}${CYAN} - DISPLAY on ${RED}$DISPLAY${NC}
 uname   -a
-cat /etc/*release*
+cat /etc/*release* &2>/dev/null
 date
