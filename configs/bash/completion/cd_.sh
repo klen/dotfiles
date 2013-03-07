373dd10fe5cdf8bb5fdb0dbbbef0c58d96dedb60
Kirill Klenov <horneds@gmail.com>
2013-03-07 10:36:49 +0800

    Fix.

--- configs/bash/completion/cd_.sh
+++ configs/bash/completion/cd_.sh
@@ -6,26 +6,18 @@ export PRJDIR=$HOME/projects
     export PRJDIR=$HOME/Dropbox/projects
 }
 
-[ -d $PRJDIR ] && {
-
-    _cdp_completion() {
-        local cur="${COMP_WORDS[COMP_CWORD]}"
-        local choices=$(find $PRJDIR -maxdepth 1 -mindepth 1 -type d)
-        COMPREPLY=( $(compgen -W "$choices" -- ${cur}) )
-    }
-    cdp () { cd $1; }
-    complete -o default -o nospace -F _cdp_completion cdp
-
+_cdp_completion() {
+    local cur="${COMP_WORDS[COMP_CWORD]}"
+    local choices=$(find $PRJDIR -maxdepth 1 -mindepth 1 -type d)
+    COMPREPLY=( $(compgen -W "$choices" -- ${cur}) )
 }
+cdp () { cd $1; }
+complete -o default -o nospace -F _cdp_completion cdp
 
-[ -d $WRKDIR ] && {
-
-    _cdw_completion() {
-        local cur="${COMP_WORDS[COMP_CWORD]}"
-        local choices=$(find $WRKDIR -maxdepth 1 -mindepth 1 -type d)
-        COMPREPLY=( $(compgen -W "$choices" -- ${cur}) )
-    }
-    cdw () { cd $1; }
-    complete -o default -o nospace -F _cdw_completion cdw
-
+_cdw_completion() {
+    local cur="${COMP_WORDS[COMP_CWORD]}"
+    local choices=$(find $WRKDIR -maxdepth 1 -mindepth 1 -type d)
+    COMPREPLY=( $(compgen -W "$choices" -- ${cur}) )
 }
+cdw () { cd $1; }
+complete -o default -o nospace -F _cdw_completion cdw
