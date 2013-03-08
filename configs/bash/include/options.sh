6e6445ed8943ac406a9dcd547c32cec20adc4b65
Kirill Klenov <horneds@gmail.com>
2013-03-08 10:55:59 +0800

    Update.

--- configs/bash/include/options.sh
+++ configs/bash/include/options.sh
@@ -12,7 +12,9 @@ export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
 
 export EDITOR=vim
+export VISUAL=vim
 export PAGER=less
+
 export HISTSIZE=10000
 export HISTIGNORE="&:cd:ls:[bf]g:exit:bash:la"
 export HISTCONTROL=erasedups:ignorespace
@@ -22,11 +24,15 @@ shopt -s cdspell
 shopt -s histappend
 shopt -s dotglob
 
+# for the love of god don't offer to autocomplete this shit
+export FIGNORE="#:~:DS_Store:.pyc:.swp:.swo"
+
 [ -x /usr/bin/lesspipe ] && eval "$(lesspipe)"
 [ -r /etc/debian_chroot ] && debian_chroot=$(cat /etc/debian_chroot)
 
 # Dont make core dump files
 ulimit -S -c 0
+
 set -o notify
 set -o noclobber
 set -o ignoreeof
