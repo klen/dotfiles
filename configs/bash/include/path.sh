417ed7de99771ca0de97fd78813f03ec902cdc27
Kirill Klenov <horneds@gmail.com>
2013-03-09 11:20:15 +0800

    npm support

--- configs/bash/include/path.sh
+++ configs/bash/include/path.sh
@@ -3,6 +3,7 @@
 
 # NodeJS integration
 [ -d $HOME/node_modules/.bin ] && PATH="$HOME/node_modules/.bin:$PATH"
+[ -d /usr/local/share/npm/bin/ ] && PATH="/usr/local/share/npm/bin/:$PATH"
 
 # Android SDK tools
 [ -d $HOME/Desktop/android-sdk-linux ] && PATH="$HOME/Desktop/android-sdk-linux/tools:$HOME/Desktop/android-sdk-linux/platform-tools:$PATH"
