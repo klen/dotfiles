e7220aadda640b581257720ac7454ceff2e5bda9
Kirill Klenov <horneds@gmail.com>
2013-04-11 16:38:23 +0800

    Fix ruby config

--- configs/bash/include/ruby.sh
+++ configs/bash/include/ruby.sh
@@ -1,13 +1,14 @@
-[ -d $HOME/.rvm ] || return
-
 setup () {
 
     # RVM support
+    [ -d $HOME/.rvm ] && {
+    
+        PATH=$PATH:$HOME/.rvm/bin
 
-    PATH=$PATH:$HOME/.rvm/bin
+        source "$HOME/.rvm/scripts/rvm"
+        source "$HOME/.rvm/scripts/completion"
 
-    source "$HOME/.rvm/scripts/rvm"
-    source "$HOME/.rvm/scripts/completion"
+    }
 
     local commands="knife vagrant cap gem"
     for n in $commands; do
