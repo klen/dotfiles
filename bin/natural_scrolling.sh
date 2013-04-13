8ab73e6145839a8f8191a35e7203d7cc64abbdcb
Kirill Klenov <horneds@gmail.com>
2013-04-13 18:10:43 +0800

    Added natural scrolling

---
+++ bin/natural_scrolling.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+DEV_NO=`xinput list | grep "ETPS/2 Elantech Touchpad" | sed 's/.*id=\([0-9]*\).*/\1/g'`
+PROP_NO=`xinput list-props $DEV_NO | grep "Synaptics Scrolling Distance" | sed 's/.*(\([0-9]*\)).*/\1/g'`
+
+xinput set-prop $DEV_NO $PROP_NO -65 -65
+nautilus -q
+nautilus -n &
