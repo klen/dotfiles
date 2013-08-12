7efd498f0d914a7bac905cf53fd6c9f476b702c7
Kirill Klenov <horneds@gmail.com>
2013-08-12 23:50:06 +0800

    Update scripts

--- bin/natural_scrolling.sh
+++ bin/natural_scrolling.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# Support a natural scrolling in Ubintu
+
 DEV_NO=`xinput list | grep "ETPS/2 Elantech Touchpad" | sed 's/.*id=\([0-9]*\).*/\1/g'`
 PROP_NO=`xinput list-props $DEV_NO | grep "Synaptics Scrolling Distance" | sed 's/.*(\([0-9]*\)).*/\1/g'`
 
