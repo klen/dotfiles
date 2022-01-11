35e46460c3b9441e7c2cf9871dc96acc6b376f5d
Kirill Klenov <horneds@gmail.com>
2022-01-11 16:41:35 +0300

    feat(bash): nvm support

--- .config/bash/include/node.sh
+++ .config/bash/include/node.sh
@@ -5,6 +5,12 @@ setup () {
         PATH=$PATH:$HOME/.meteor
     }
 
+    # NVM support
+    [ -s "/usr/local/opt/nvm/nvm.sh" ] && {
+        source "/usr/local/opt/nvm/nvm.sh"
+        [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && source "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
+    }
+
 }
 
 setup && unset setup
