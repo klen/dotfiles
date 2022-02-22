5070ec5ea8927b34224297bc1db057195b5aaa6e
Kirill Klenov <horneds@gmail.com>
2022-02-22 12:42:54 +0300

    feat: disable cowsay for nvim

--- .config/bash/login.sh
+++ .config/bash/login.sh
@@ -4,9 +4,11 @@
 # echo    -e ${CYAN}This is BASH ${RED}${BASH_VERSION%.*}${CYAN} - DISPLAY on ${RED}$DISPLAY${NC}
 # uname   -a
 # cat /etc/lsb-release 2&>/dev/null
-_is_cmd_exist cowsay && _is_cmd_exist fortune &&  fortune -s | cowsay -f $XDG_CONFIG_HOME/girl.cow 2>/dev/null
-# _is_cmd_exist landscape-sysinfo && landscape-sysinfo
-_is_cmd_exist lastlog && lastlog -u $USER
+if [ -z "$NVIM_LISTEN_ADDRESS" ]; then
+    _is_cmd_exist cowsay && _is_cmd_exist fortune &&  fortune -s | cowsay -f $XDG_CONFIG_HOME/girl.cow 2>/dev/null
+    # _is_cmd_exist landscape-sysinfo && landscape-sysinfo
+    _is_cmd_exist lastlog && lastlog -u $USER
+fi
 
 # Information about screens
 if [ ! -z "$WINDOW" -a ! "$SHOWED_SCREEN_MESSAGE" = "true" ]; then
