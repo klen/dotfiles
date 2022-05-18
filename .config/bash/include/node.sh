f130be8849c16ae9d4c22d73f6a28490763081c9
Kirill Klenov <horneds@gmail.com>
2022-05-18 10:55:47 +0300

    feat: tune

--- .config/bash/include/node.sh
+++ .config/bash/include/node.sh
@@ -1,13 +1,10 @@
 setup () {
 
-    # RVM support
-    [ -d $HOME/.meteor ] && {
-        PATH=$PATH:$HOME/.meteor
-    }
-
     # NVM support
     [ -s "/usr/local/opt/nvm/nvm.sh" ] && {
+        alias manpath=false  # https://github.com/nvm-sh/nvm/issues/2747
         source "/usr/local/opt/nvm/nvm.sh"
+        unalias manpath
         # [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && source "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
     }
 
