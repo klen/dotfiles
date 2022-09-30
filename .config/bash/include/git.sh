e2133cc5ee13b8dfdd47ca21b3bc0ce5867f11b1
Kirill Klenov <horneds@gmail.com>
2022-09-30 23:15:15 +0300

    feat: better support for git

--- .config/bash/include/git.sh
+++ .config/bash/include/git.sh
@@ -1,10 +1,10 @@
 _is_cmd_exist git && {
 
-    # source $XDG_CONFIG_HOME/bash/completion/git.sh
-    # source $XDG_CONFIG_HOME/bash/completion/git-flow.sh
+    source $XDG_CONFIG_HOME/bash/completion/git-completion.sh
+    source $XDG_CONFIG_HOME/bash/completion/git-flow.sh
+    source $XDG_CONFIG_HOME/bash/completion/git-promt.sh
 
     GIT_PS1_SHOWDIRTYSTATE=1
     GIT_PS1_SHOWSTASHSTATE=1
     PS1=$PS1$GREEN'$(__git_ps1 " (%s)")'$NC
-
 }
