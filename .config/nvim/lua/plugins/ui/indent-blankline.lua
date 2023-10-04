e706e78b40996ac228fd9112c058f9af05657b51
Kirill Klenov <horneds@gmail.com>
2023-10-04 18:10:03 +0300

    feat(smartcd): tune venv

--- .config/nvim/lua/plugins/ui/indent-blankline.lua
+++ .config/nvim/lua/plugins/ui/indent-blankline.lua
@@ -2,11 +2,10 @@
 -- https://github.com/lukas-reineke/indent-blankline.nvim
 return {
   "lukas-reineke/indent-blankline.nvim",
-  config = function()
-    require("indent_blankline").setup {
-      use_treesitter = true,
-      show_first_indent_level = false,
-      filetype_exclude = { "", "help", "packer", "starter" },
-    }
-  end,
+  main = "ibl",
+  opts = {
+    exclude = {
+      filetypes = { "", "help", "packer", "starter" },
+    },
+  },
 }
