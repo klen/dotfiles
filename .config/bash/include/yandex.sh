176469d087dae2299a9724d3bf0159fbabb70b78
Kirill Klenov <horneds@gmail.com>
2024-07-04 12:38:20 +0300

    feat: add yandex cli

---
+++ .config/bash/include/yandex.sh
@@ -0,0 +1,6 @@
+# The next line updates PATH for Yandex Cloud CLI.
+if [ -f '/Users/horneds/yandex-cloud/path.bash.inc' ]; then source '/Users/horneds/yandex-cloud/path.bash.inc'; fi
+
+# The next line enables shell command completion for yc.
+if [ -f '/Users/horneds/yandex-cloud/completion.bash.inc' ]; then source '/Users/horneds/yandex-cloud/completion.bash.inc'; fi
+
