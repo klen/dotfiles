1f6fefb1c48fddd6c2a2d6585e286448f5da6810
Kirill Klenov <horneds@gmail.com>
2025-06-14 01:20:24 +0700

    feat: Add support for OrbStack and improve Docker command aliases 🐳 🧞

--- .config/bash/include/docker.sh
+++ .config/bash/include/docker.sh
@@ -1,20 +1,43 @@
-# Support docker
-hash docker 2>/dev/null && {
+# Support for OrbStack
+if [[ -d "$HOME/.orbstack" ]]; then
+  source ~/.orbstack/shell/init.bash 2>/dev/null || :
+fi
 
-    dps () {
-        docker ps --format="table {{.Names}}\t{{.Status}}" $@
-    }
+# Support for Docker commands and aliases
+if command -v docker >/dev/null 2>&1; then
 
-    dlogs () {
-        docker logs -f --tail 1000 $@
-    }
+  # List all Docker containers
+  dps() {
+    docker ps --format="table {{.Names}}\t{{.Image}}\t{{.Status}}\t{{.Ports}}" "$@"
+  }
 
-    dexec () {
-        docker exec -it $@
-    }
+  # View logs of a running container
+  dlogs() {
+    if [ -z "$1" ]; then
+      echo "Usage: dlogs <container-name>"
+      return 1
+    fi
+    docker logs -f --tail=1000 "$@"
+  }
 
-    if [ "$OS" == "Darwin" ]; then
-      export DOCKER_HOST=unix://"$HOME/.docker/run/docker.sock"
+  # Exec into a running container
+  dexec() {
+    if [ -z "$1" ]; then
+      echo "Usage: dexec <container-name> [shell]"
+      return 1
     fi
 
-}
+    local shell="${2:-/bin/bash}"
+    docker exec -it "$1" "$shell"
+  }
+
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    if [[ -S "$HOME/.orbstack/run/docker.sock" ]]; then
+      export DOCKER_HOST="unix://$HOME/.orbstack/run/docker.sock"
+    elif [[ -S "$HOME/.docker/run/docker.sock" ]]; then
+      export DOCKER_HOST="unix://$HOME/.docker/run/docker.sock"
+    else
+      unset DOCKER_HOST
+    fi
+  fi
+fi
