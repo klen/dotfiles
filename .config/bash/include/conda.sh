f3b31c1adc1618d9a56d4648fbdd9e66a4060d94
Kirill Klenov <horneds@gmail.com>
2025-06-22 21:26:59 +0700

    feat: add Conda support to bashrc 🚀
    This commit introduces support for Conda in the `bashrc` configuration. A new file `.config/bash/include/conda.sh` has been added to source the Conda environment setup script when it is installed via Homebrew. The `bashrc` file has also been updated to include this new script in the initialization process, ensuring that the Conda environment is properly set up when a new shell session starts.

---
+++ .config/bash/include/conda.sh
@@ -0,0 +1,4 @@
+# Support for Conda (insalled via Homebrew)
+if [ -f "/opt/homebrew/Caskroom/miniforge/base/etc/profile.d/conda.sh" ]; then
+  . "/opt/homebrew/Caskroom/miniforge/base/etc/profile.d/conda.sh"
+fi
