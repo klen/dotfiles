eeb23fd4f01aab867cc1c4836696f0242008f70b
Kirill Klenov <horneds@gmail.com>
2025-06-30 12:56:02 +0700

    feat: add welcome message for tmux sessions ✨
    This commit adds a new function `show_welcome()` that displays a welcome message using `cowsay` and `fortune`. The function is called when logging in via tmux or a regular shell session, but only once per session. This ensures that the welcome message is shown only when a new tmux window or pane is opened, preventing it from being displayed multiple times within the same session.

--- .config/bash/login.sh
+++ .config/bash/login.sh
@@ -1,27 +1,38 @@
 # Login screen
 # ============
 
-# echo    -e ${CYAN}This is BASH ${RED}${BASH_VERSION%.*}${CYAN} - DISPLAY on ${RED}$DISPLAY${NC}
-# uname   -a
-# cat /etc/lsb-release 2&>/dev/null
-if [ -z "$NVIM_LISTEN_ADDRESS" ]; then
-    hash cowsay 2>/dev/null && hash fortune 2>/dev/null && fortune -s | cowsay -f $XDG_CONFIG_HOME/bash/girl.cow 2>/dev/null
-    # hash landscape-sysinfo 2>/dev/null && landscape-sysinfo
-    # hash lastlog 2>/dev/null && lastlog -u $USER
+echo -e ${CYAN}This is BASH ${RED}${BASH_VERSION%.*}${CYAN} - DISPLAY on ${RED}$DISPLAY${NC}
+uname -a
+cat /etc/lsb-release 2 &>/dev/null
+show_welcome() {
+  hash cowsay 2>/dev/null && hash fortune 2>/dev/null &&
+    fortune -s | cowsay -f "$XDG_CONFIG_HOME/bash/girl.cow" 2>/dev/null
+}
+
+if [ -n "$TMUX" ]; then
+  if ! tmux show-environment SHOWED_WELCOME >/dev/null 2>&1; then
+    show_welcome
+    tmux set-environment SHOWED_WELCOME "true"
+  fi
+else
+  if [ -z "$SHOWED_WELCOME" ]; then
+    show_welcome
+    export SHOWED_WELCOME="true"
+  fi
 fi
 
 # Information about screens
 if [ ! -z "$WINDOW" -a ! "$SHOWED_SCREEN_MESSAGE" = "true" ]; then
-    detached_screens=`screen -list | grep Detached`
-    echo -e $YELLOW
-    if [ ! -z "$detached_screens" ]; then
-        echo "+---------------------------------------+"
-        echo "| Detached screens are available:       |"
-        echo "$detached_screens"
-        echo "+---------------------------------------+"
-    else
-        echo "[ screen is activated ]"
-    fi
-    echo -e $NC
-    export SHOWED_SCREEN_MESSAGE="true"
+  detached_screens=$(screen -list | grep Detached)
+  echo -e $YELLOW
+  if [ ! -z "$detached_screens" ]; then
+    echo "+---------------------------------------+"
+    echo "| Detached screens are available:       |"
+    echo "$detached_screens"
+    echo "+---------------------------------------+"
+  else
+    echo "[ screen is activated ]"
+  fi
+  echo -e $NC
+  export SHOWED_SCREEN_MESSAGE="true"
 fi
