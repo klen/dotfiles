ac743b75d1ed34021acc09e09d2c54d586c0743f
Kirill Klenov <horneds@gmail.com>
2025-07-02 02:02:56 +0700

    feat: add tmuxp support and alias ✨
    This commit adds `tmuxp` support for managing tmux sessions more efficiently. It includes:
    
    1. A new bash script in `.config/bash/include/tmux.sh` that defines an alias `tml` for loading tmuxp configurations and sets up tab completion for this alias.
    2. Updates to `.config/bash/aliases.sh` to include the new `tml` alias.
    3. Modifications to `.config/tmux/plugins.conf` to enable automatic saving and restoring of tmux sessions using `tmux-continuum`.
    4. An update to the `setup/install.mac.yml` file to include `tmuxp` in the list of Homebrew packages to install.
    
    These changes enhance the tmux experience by allowing users to easily manage complex tmux sessions with `tmuxp` and ensuring session persistence with `tmux-continuum`.

--- .config/bash/aliases.sh
+++ .config/bash/aliases.sh
@@ -1,17 +1,17 @@
 # ---------------
 # Better terminal
-alias cic='set completion-ignore-case On'   # cic:          Make tab-completion case-insensitive
-alias cp='cp -iv'                           # Preferred 'cp' implementation
-alias fix_stty='stty sane'                  # fix_stty:     Restore terminal settings when screwed up
-alias fix_term='echo -e "\033c"'            # fix_term:     Reset the conosle.  Similar to the reset command
-alias mkdir='mkdir -pv'                     # Preferred 'mkdir' implementation
-alias mv='mv -iv'                           # Preferred 'mv' implementation
-alias nano='nano -W'                        # Preferred 'nano' implementation
-alias path='echo -e ${PATH//:/\\n}'         # path:         Echo all executable Paths
+alias cic='set completion-ignore-case On' # cic:          Make tab-completion case-insensitive
+alias cp='cp -iv'                         # Preferred 'cp' implementation
+alias fix_stty='stty sane'                # fix_stty:     Restore terminal settings when screwed up
+alias fix_term='echo -e "\033c"'          # fix_term:     Reset the conosle.  Similar to the reset command
+alias mkdir='mkdir -pv'                   # Preferred 'mkdir' implementation
+alias mv='mv -iv'                         # Preferred 'mv' implementation
+alias nano='nano -W'                      # Preferred 'nano' implementation
+alias path='echo -e ${PATH//:/\\n}'       # path:         Echo all executable Paths
 alias rm='rm -i'
-alias show_options='shopt'                  # Show_options: display bash options settings
-alias src='source ~/.bashrc'                # src:          Reload .bashrc file
-alias wget='wget -c'                        # Preferred 'wget' implementation (resume download)
+alias show_options='shopt'   # Show_options: display bash options settings
+alias src='source ~/.bashrc' # src:          Reload .bashrc file
+alias wget='wget -c'         # Preferred 'wget' implementation (resume download)
 
 # Personal
 # connect to remote host
@@ -27,5 +27,6 @@ alias ll='ls $LS_OPTIONS -goAh'
 alias la='ll $LS_OPTIONS -FlAh'
 
 # others
+alias tml='tmuxp load -y'
 alias vi='vim'
 alias myip='dig +short txt ch whoami.cloudflare @1.0.0.1'
---
+++ .config/bash/include/tmux.sh
@@ -0,0 +1,13 @@
+hash tmuxp 2>/dev/null && {
+  alias tml='tmuxp load -y'
+
+  _tml_completions() {
+    local curr_word
+    curr_word="${COMP_WORDS[COMP_CWORD]}"
+
+    # Получаем список tmuxp configs (по умолчанию в ~/.tmuxp/)
+    COMPREPLY=($(compgen -W "$(ls ~/.tmuxp | sed 's/\.yaml$//' | sed 's/\.yml$//')" -- "$curr_word"))
+  }
+
+  complete -F _tml_completions tml
+}
